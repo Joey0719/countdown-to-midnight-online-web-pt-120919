6d8afd78bc5ec0d334f0530f0896054286daaa4d
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-12-10 22:02:38 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -9,17 +9,3 @@ describe 'LOOPS' do
     it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
       expect { countdown(10) }.to output(countdown_output).to_stdout
     end
-
-    it 'returns HAPPY NEW YEAR!' do
-      expect(countdown(12)).to eq "HAPPY NEW YEAR!"
-    end
-  end
-
-  describe '#countdown_with_sleep' do
-    it 'should take at least 5 seconds to execute' do
-      runtime = Benchmark.measure { countdown_with_sleep(5) }
-      expect(runtime.real.to_i >= 5).to be true
-    end
-  end
-
-end
